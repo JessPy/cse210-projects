33bb37e552217d0e256f4a23a4ea11f6e2643291
JessPy <jbodartguimaraes@gmail.com>
2024-01-08 12:16:56 -0300

    Variables, Input and Output learning material

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,18 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        string first = Console.ReadLine();
+
+        Console.Write("Whar is your last name? ");
+        string last = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {last}, {first} {last}.");
+
+    
     }
 }
