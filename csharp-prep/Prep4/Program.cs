13257df47aa4f3faa48b7fcc5fa04c22d71880b3
JessPy <jbodartguimaraes@gmail.com>
2024-01-09 12:59:34 -0300

    List adder

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> num = new List<int>();
+        int numUser = -1;
+        
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        
+        do
+        {
+            Console.Write("Enter number: ");
+            numUser = int.Parse(Console.ReadLine());
+    
+            if (!(numUser == 0))
+            {
+                num.Add(numUser);
+            }
+        }while(numUser != 0);
+        
+        float sum = 0;
+        foreach (int element in num)
+        {
+            sum += element;
+        }
+        
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The avarage is: {sum/num.Count}");
     }
 }
