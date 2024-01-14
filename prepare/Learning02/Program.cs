64b80a94e4c0dc61e2f58c50b8c8b9da2f7d83e0
JessPy <jbodartguimaraes@gmail.com>
2024-01-14 20:41:10 -0300

    job update

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -7,13 +7,13 @@ class Program
     {
         Job job1 = new Job();
         job1._company = "Nissei";
-        job1._jobTitle = "Assistente de farmácia";
+        job1._jobTitle = "Assistant";
         job1._startYear = 2019;
         job1._endYear = 2023;
 
         Job job2 = new Job();
         job2._company = "PUCPR";
-        job2._jobTitle = "Iniciação Científica";
+        job2._jobTitle = "Undergraduate Research Project";
         job2._startYear = 2023;
         job2._endYear = 2024;
         
